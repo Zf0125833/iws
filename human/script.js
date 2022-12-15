1a4d29ab1b4bf7ca22f03482b8ce9f8e25d3930f
John <66988742+Zf0125833@users.noreply.github.com>
2022-12-16 02:25:04 +0400

    Add files via upload

--- human/script.js
+++ human/script.js
@@ -4,27 +4,26 @@ const huMan = document.querySelector(".hu-man");
 const hHead = document.querySelector(".h-head");
 const hQuote = document.querySelector(".h-quote");
 
-let headId = headArr.length;
-let quoteId = quotesArr.length;
-
 function randomElem(arr, elem) {
     const rI = Math.floor(Math.random() * arr.length);
     elem.src = arr[rI];
 }
 
-function nextHead() {
-    headId++;
-    if (headId >= headArr.length) headId = 0;
-    return hHead.src = headArr[headId]
-}
+huMan.addEventListener("click", () => randomElem(headArr, hHead) ?? randomElem(quotesArr, hQuote));
 
-function nextQuotes() {
-    quoteId++;
-    if (quoteId >= quotesArr.length) quoteId = 0;
-    return hQuote.src = quotesArr[quoteId]
-}
+randomElem(headArr, hHead) ?? randomElem(quotesArr, hQuote);
 
-randomElem(headArr, hHead);
-randomElem(quotesArr, hQuote);
+// let headId = headArr.length;
+// let quoteId = quotesArr.length;
 
-huMan.addEventListener("click", () => { nextHead(); nextQuotes(); })
+// function nextHead() {
+//     headId++;
+//     if (headId >= headArr.length) headId = 0;
+//     return hHead.src = headArr[headId]
+// }
+
+// function nextQuotes() {
+//     quoteId++;
+//     if (quoteId >= quotesArr.length) quoteId = 0;
+//     return hQuote.src = quotesArr[quoteId]
+// }
