2849ad932517f8e7d3666e11b4c5369f7185ebd4
John <66988742+Zf0125833@users.noreply.github.com>
2025-07-16 14:48:55 +0400

    Update script.js

--- ip/static/script.js
+++ ip/static/script.js
@@ -247,7 +247,6 @@ async function detectHardwareAndEnvironment() {
                 <div class="box-nav links"><div><strong>Browserleaks:</strong></div> <div><a href="https://browserleaks.com/" target="_balnk">browserleaks.com</a></div></div>
 		<div class="box-nav links"><div><strong>whatismyipaddress:</strong></div> <div><a href="https://whatismyipaddress.com/" target="_balnk">whatismyipaddress.com</a></div></div>
                 <div class="box-nav links"><div><strong>Whoer:</strong></div> <div><a href="https://whoer.net/" target="_balnk">whoer.net</a></div></div>
-                <div class="box-nav links"><div><strong>Dnsleaktest:</strong></div> <div><a href="https://dnsleaktest.com/" target="_balnk">dnsleaktest.com</a></div></div>
                 
                 <h2>internet Speed Test</h2>
                 <div class="box-nav links"><div><strong>speedtest:</strong></div> <div><a href="https://www.speedtest.net" target="_balnk">speedtest.net</a></div></div>
